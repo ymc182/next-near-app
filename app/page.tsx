4c5ed27ef5aae6ca8515e5325eff34070543dc97
ymc182 <ymc182@gmail.com>
2023-01-04 15:30:16 +1100

    page v1

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ export default function Home() {
 		<div className=" h-screen ">
 			<div className="landing-image-area">
 				<div className="landing-image-area-image-large"></div>
-				<div className="landing-image-area-image-small"></div>
+				<div className="landing-image-area-image-small "></div>
 			</div>
 			<div className="landing-title-area">
 				<div className="landing-title-text-main">first glance</div>
 				<div className="landing-title-text-title">text title</div>
+				<div className="landing-title-text-company">By Company Name</div>
 			</div>
+			<div className="landing-vertical-text">ANY PROJECT</div>
 		</div>
 	);
 }
